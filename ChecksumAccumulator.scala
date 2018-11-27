a9867436dd79d6a975550ac78b6e5fd4342d24b8
mum-chen <mum-chen@qq.com>
2018-11-27 15:33:44 +0800

    feat: basic oop

---
+++ ChecksumAccumulator.scala
@@ -0,0 +1,42 @@
+import scala.collection.mutable
+
+class ChecksumAccumulator {
+  private var sum = 0
+  def add(b: Byte): Unit = { sum += b }
+  def checksum(): Int = ~(sum & 0xff) + 1
+}
+
+class ChecksumAccumulatorVerbose {
+  private var sum = 0
+  def add(b: Byte): Unit = {
+    /* Error, the parameter in scala is `val`
+    b  = 1
+    */
+    sum += b
+  }
+
+  def checksum(): Int = {
+    return ~(sum & 0xff) + 1
+  }
+}
+
+/*
+ * companion class
+ * A way to support static method & static class
+ * This object must in the same source file with class
+ */
+object ChecksumAccumulator {
+  private val cache = mutable.Map.empty[String, Int]
+
+  def calculate(s: String): Int =
+    if (cache.contains(s))
+      cache(s)
+    else {
+      val acc = new ChecksumAccumulator
+      for (c <- s)
+        acc.add(c.toByte)
+      val cs = acc.checksum()
+      cache += (s -> cs)
+      cs
+    }
+}
--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
-scala hello_01.scala wor so hard
+scala hello_01.scala work so hard
 scala basic_02.scala
 scala countchars_03.scala countchars_03.scala
+
+scalac ChecksumAccumulator.scala Summer.scala
+scala Summer Work so hard
+
+scalac FallWinterSpringSummer.scala
+scala FallWinterSpringSummer
